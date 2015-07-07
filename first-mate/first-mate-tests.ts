20fe185948e07a2f7e30fb9fd9eddcbb973a530b
Vadim Macagon <vadim.macagon@gmail.com>
2015-07-07 11:18:00 +0700

    Combine the two types of possible results from tokenizeLine into one

--- first-mate/first-mate-tests.ts
+++ first-mate/first-mate-tests.ts
@@ -1,13 +1,15 @@
 /// <reference path="./first-mate.d.ts" />
 
-// The following line only works in TypeScript 1.5
-//import { GrammarRegistry, Grammar } from "first-mate";
-// DefinitelyTyped is still using TypeScript 1.4 to run tests
-// so until they upgrade we have to do the following instead
 import firstMate = require('first-mate');
 var GrammarRegistry = firstMate.GrammarRegistry;
 var Grammar = firstMate.GrammarRegistry;
+type IToken = AtomFirstMate.IToken;
+// The import and type aliasing above can be done more concisely in TypeScript 1.5+:
+//import { GrammarRegistry, Grammar, IToken } from "first-mate";
 
 var registry = new GrammarRegistry({ maxTokensPerLine: 100 });
-var grammar = registry.loadGrammarSync("path/to/grammar");
-var tokens = grammar.tokenizeLines("Just another line");
+var grammar = registry.loadGrammarSync('javascript.json');
+var result = grammar.tokenizeLine('var text = "hello world";');
+result.tokens.forEach((token) => {
+	console.log("Token text: '" + token.value + "' with scopes: " + token.scopes);
+});
--- first-mate/first-mate.d.ts
+++ first-mate/first-mate.d.ts
@@ -6,84 +6,103 @@
 /// <reference path="../event-kit/event-kit.d.ts" />
 
 declare module AtomFirstMate {
-  type Disposable = EventKit.Disposable;
+	type Disposable = AtomEventKit.Disposable;
 
-  interface IToken {
-    value: string;
-    scopes: string[];
-  }
+	interface IToken {
+		value: string;
+		scopes: string[];
+	}
 
-  /** Instance side of TokenizeLineResult class. */
-  interface TokenizeLineResult {
-    tokens: IToken[];
-  }
+	/** Result returned by `Grammar.tokenizeLine`. */
+	interface TokenizeLineResult {
+		/** Text that was tokenized. */
+		line: string;
+		tags: any[];
+		/**
+		 * This is a dynamic property that will only be available if `Grammar.tokenizeLine` was called
+		 * with `compatibilityMode` set to `true` (the default).
+		 */
+		tokens?: IToken[];
+		/**
+		 * The tokenized state at the end of the line. This should be passed back into `tokenizeLine`
+		 * when tokenizing the next line in the file/buffer.
+		 */
+		ruleStack: Rule[]
+	}
 
-  // This is another possible result type for `Grammar.tokenizeLine()`,
-  // not to be confused with `TokenizeLineResult`.
-  interface ITokenizeLineResult {
-    line: string;
-    tags: any[];
-    ruleStack: Rule[];
-  }
+	/** Instance side of Rule class. */
+	interface Rule {
+	}
 
-  /** Instance side of Rule class. */
-  interface Rule {
-  }
+	/** Static side of Grammar class. */
+	interface GrammarStatic {
+		prototype: Grammar;
+		new (registry: GrammarRegistry, options?: any): Grammar;
+	}
 
-  /** Static side of Grammar class. */
-  interface GrammarStatic {
-    prototype: Grammar;
-    new (registry: GrammarRegistry, options?: any): Grammar;
-  }
+	/** Instance side of Grammar class. */
+	interface Grammar {
+		constructor: GrammarStatic;
+		onDidUpdate(callback: Function): Disposable;
+		/**
+		 * Tokenizes all lines in a string.
+		 *
+		 * @param text A string containing one or more lines.
+		 * @return An array of token arrays, one token array per line.
+		 */
+		tokenizeLines(text: string): Array<Array<IToken>>;
+		/**
+		 * Tokenizes a line of text.
+		 *
+		 * @param line Text to be tokenized.
+		 * @param firstLine Indicates whether `line` is the first line in the file/buffer,
+		 *                  defaults to `false`.
+		 * @param compatibilityMode `true` by default.
+		 * @return An object containing tokens for the given line.
+		 */
+		tokenizeLine(
+			line: string, ruleStack?: Rule[], firstLine?: boolean, compatibilityMode?: boolean
+		): TokenizeLineResult;
+	}
 
-  /** Instance side of Grammar class. */
-  interface Grammar {
-    constructor: GrammarStatic;
-    onDidUpdate(callback: Function): Disposable;
-    tokenizeLines(text: string): Array<Array<IToken>>;
-    tokenizeLine(
-      line: string, ruleStack?: Rule[], firstLine?: boolean, compatibilityMode?: boolean
-    ): ITokenizeLineResult | TokenizeLineResult;
-  }
+	/** Grammar that tokenizes lines of text. */
+	var Grammar: GrammarStatic;
 
-  /** Grammar that tokenizes lines of text. */
-  var Grammar: GrammarStatic;
+	/** Static side of GrammarRegistry class. */
+	interface GrammarRegistryStatic {
+		prototype: GrammarRegistry;
+		new (options?: { maxTokensPerLine: number }): GrammarRegistry;
+	}
 
-  /** Static side of GrammarRegistry class. */
-  interface GrammarRegistryStatic {
-    prototype: GrammarRegistry;
-    new (options?: { maxTokensPerLine: number }): GrammarRegistry;
-  }
+	/** Instance side of GrammarRegistry class. */
+	interface GrammarRegistry {
+		constructor: GrammarRegistryStatic;
 
-  /** Instance side of GrammarRegistry class. */
-  interface GrammarRegistry {
-    constructor: GrammarRegistryStatic;
+		// Event Subscription
 
-    // Event Subscription
+		onDidAddGrammar(callback: (grammar: Grammar) => void): Disposable;
+		onDidUpdateGrammar(callback: (grammar: Grammar) => void): Disposable;
 
-    onDidAddGrammar(callback: (grammar: Grammar) => void): Disposable;
-    onDidUpdateGrammar(callback: (grammar: Grammar) => void): Disposable;
+		// Managing Grammars
 
-    // Managing Grammars
+		getGrammars(): Grammar[];
+		grammarForScopeName(scopeName: string): Grammar;
+		addGrammar(grammar: Grammar): Disposable;
+		removeGrammarForScopeName(scopeName: string): Grammar;
+		readGrammarSync(grammarPath: string): Grammar;
+		readGrammar(grammarPath: string, callback: (error: Error, grammar: Grammar) => void): void;
+		loadGrammarSync(grammarPath: string): Grammar;
+		loadGrammar(grammarPath: string, callback: (error: Error, grammar: Grammar) => void): void;
+		grammarOverrideForPath(filePath: string): Grammar;
+		setGrammarOverrideForPath(filePath: string, scopeName: string): Grammar;
+		clearGrammarOverrides(): void;
+		selectGrammar(filePath: string, fileContents: string): Grammar;
+	}
 
-    getGrammars(): Grammar[];
-    grammarForScopeName(scopeName: string): Grammar;
-    addGrammar(grammar: Grammar): Disposable;
-    removeGrammarForScopeName(scopeName: string): Grammar;
-    readGrammarSync(grammarPath: string): Grammar;
-    readGrammar(grammarPath: string, callback: (error: Error, grammar: Grammar) => void): void;
-    loadGrammarSync(grammarPath: string): Grammar;
-    loadGrammar(grammarPath: string, callback: (error: Error, grammar: Grammar) => void): void;
-    grammarOverrideForPath(filePath: string): Grammar;
-    setGrammarOverrideForPath(filePath: string, scopeName: string): Grammar;
-    clearGrammarOverrides(): void;
-    selectGrammar(filePath: string, fileContents: string): Grammar;
-  }
-
-  /** Registry containing one or more grammars. */
-  var GrammarRegistry: GrammarRegistryStatic;
+	/** Registry containing one or more grammars. */
+	var GrammarRegistry: GrammarRegistryStatic;
 }
 
 declare module 'first-mate' {
-  export = AtomFirstMate;
+	export = AtomFirstMate;
 }
