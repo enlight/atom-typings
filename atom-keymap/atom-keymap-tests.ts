2e581982297117246967af8a4c91daba0324d84d
Vadim Macagon <vadim.macagon@gmail.com>
2015-07-07 11:29:18 +0700

    Convert spaces to tabs

--- atom-keymap/atom-keymap-tests.ts
+++ atom-keymap/atom-keymap-tests.ts
@@ -1,25 +1,24 @@
 /// <reference path="./atom-keymap.d.ts" />
 
-// The following line only works in TypeScript 1.5
-//import { KeymapManager } from "atom-keymap";
-// DefinitelyTyped is still using TypeScript 1.4 to run tests
-// so until they upgrade we have to do the following instead
 import atomKeymap = require('atom-keymap');
 var KeymapManager = atomKeymap.KeymapManager;
+type ICompleteMatchEvent = AtomKeymap.ICompleteMatchEvent;
+// The import and type aliasing above can be done more concisely in TypeScript 1.5+:
+//import { KeymapManager, ICompleteMatchEvent } from "atom-keymap";
 
 var manager = new KeymapManager();
 manager.add('some/unique/path', {
-  '.workspace': {
-    'ctrl-x': 'package:do-something',
-    'ctrl-y': 'package:do-something-else'
-  },
-  '.mini.editor': {
-    'enter': 'core:confirm'
-  }
+    '.workspace': {
+        'ctrl-x': 'package:do-something',
+        'ctrl-y': 'package:do-something-else'
+    },
+    '.mini.editor': {
+        'enter': 'core:confirm'
+    }
 });
 
-manager.onDidMatchBinding((event: AtomKeymap.ICompleteMatchEvent): void => {
-  console.log(event.binding.command);
+manager.onDidMatchBinding((event: ICompleteMatchEvent): void => {
+    console.log(event.binding.command);
 })
 
 manager.destroy();
