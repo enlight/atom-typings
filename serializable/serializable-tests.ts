e37b2a21be589b6066c283e7a0c63ea3b6c14524
Vadim Macagon <vadim.macagon@gmail.com>
2015-07-16 22:19:42 +0700

    Normalize indentation (spaces -> tabs)

--- serializable/serializable-tests.ts
+++ serializable/serializable-tests.ts
@@ -5,49 +5,49 @@ import { Serializable, ISerializable, ISerializedState, IParamsSerializer } from
 // Easiest way to use the `Serializable` mixin is not to use it as a mixin, because mixins in
 // TypeScript are a pain. Instead just subclass it.
 class Pilot extends Serializable implements IParamsSerializer {
-  constructor(public name: string, public rank: string) {
-    super();
-  }
+	constructor(public name: string, public rank: string) {
+		super();
+	}
 
-  serializeParams(): any {
-    return {
-      name: this.name,
-      rank: this.rank
-    };
-  }
+	serializeParams(): any {
+		return {
+			name: this.name,
+			rank: this.rank
+		};
+	}
 }
 
 interface PlaneParams {
-  engines: number;
-  pilot: any;
+	engines: number;
+	pilot: any;
 }
 
 // When you can't just subclass `Serializable`, you'll have to use the `ISerializable` interface.
 class Plane implements ISerializable {
-  constructor(public engines: number, public pilot?: Pilot) {
-    if (pilot === undefined) {
-      this.pilot = new Pilot('John', 'Captain');
-    }
-  }
+	constructor(public engines: number, public pilot?: Pilot) {
+		if (pilot === undefined) {
+			this.pilot = new Pilot('John', 'Captain');
+		}
+	}
 
-  serializeParams(): PlaneParams {
-    return {
-      engines: this.engines,
-      pilot: this.pilot.serialize()
-    };
-  }
+	serializeParams(): PlaneParams {
+		return {
+			engines: this.engines,
+			pilot: this.pilot.serialize()
+		};
+	}
 
-  deserializeParams(params: PlaneParams): PlaneParams {
-    params.pilot = Pilot.deserialize(params.pilot);
-    return params;
-  }
+	deserializeParams(params: PlaneParams): PlaneParams {
+		params.pilot = Pilot.deserialize(params.pilot);
+		return params;
+	}
 
-  // these are declared here to satisfy the Serializable mixin interface,
-  // but the implementation will actually be provided by the mixin itself
-  static registerDeserializers: (...deserializers: Function[]) => void;
-  static registerDeserializer: (deserializer: Function) => void;
-  static deserialize: {<T>(state: ISerializedState, params?: any): T};
-  serialize: () => ISerializedState;
+	// these are declared here to satisfy the Serializable mixin interface,
+	// but the implementation will actually be provided by the mixin itself
+	static registerDeserializers: (...deserializers: Function[]) => void;
+	static registerDeserializer: (deserializer: Function) => void;
+	static deserialize: {<T>(state: ISerializedState, params?: any): T};
+	serialize: () => ISerializedState;
 }
 
 Serializable.includeInto(Plane);
